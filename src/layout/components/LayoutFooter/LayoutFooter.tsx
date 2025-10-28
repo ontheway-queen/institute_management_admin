c6e463828298cacffb45ce68131c524731cb86f8
showrav <showrav.m360ict@gamil.com>
2025-10-28 18:42:52 +0600

    fixed upload error

--- src/layout/components/LayoutFooter/LayoutFooter.tsx
+++ src/layout/components/LayoutFooter/LayoutFooter.tsx
@@ -1,5 +1,5 @@
-import { Card, Col, Row, Typography } from 'antd';
-import React from 'react';
+import { Card, Col, Row, Typography } from "antd";
+import React from "react";
 
 const LayoutFooter: React.FC = () => {
   const year: number = new Date().getFullYear();
@@ -7,18 +7,18 @@ const LayoutFooter: React.FC = () => {
   return (
     <>
       <Card>
-        <Row justify={{ xs: 'center', md: 'space-between' }}>
+        <Row justify={{ xs: "center", md: "space-between" }}>
           <Col></Col>
           <Col>
-            {' '}
+            {" "}
             <Typography.Paragraph style={{ margin: 0 }}>
-              Copyright © {year} <strong> Institute Management </strong>by
-              M360ICT. All rights reserved.
+              Copyright © {year} <strong>Polytech </strong>by M360ICT and
+              Desktop IT (JV). All rights reserved.
             </Typography.Paragraph>
           </Col>
           <Col>
-            {' '}
-            <Typography.Link type='secondary'>Support</Typography.Link>
+            {" "}
+            <Typography.Link type="secondary">Support</Typography.Link>
           </Col>
         </Row>
       </Card>
